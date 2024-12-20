6f671b97b407017585e1c20c3d4df44d50afebe5
Rajat-Kush <rajatkshwh131@gmail.com>
2024-12-20 11:28:58 +0530

    Files Upload

---
+++ Assets/sripts/linkmanager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linkmanager : MonoBehaviour
+{
+    public void openLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/kushwaha-rajat-kamalakant");
+    }
+
+    public void openInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/rajatking_official/");
+    }
+    public void openDiscord()
+    {
+        Application.OpenURL("https://www.discord.com/users/754721264597467167");
+    }
+    public void openX()
+    {
+        Application.OpenURL("https://x.com/rajatking3040?t=FI-_QefP3AOysUI49LKqmA&s=09");
+    }
+    public void openGithub()
+    {
+        Application.OpenURL("https://github.com/Rajat-Kush");
+    }
+}
